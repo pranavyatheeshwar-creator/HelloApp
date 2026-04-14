47f4866391d0eb075e21d3ba71ce02badfcec7cb
SKPradeepp <skpiitmember2008@gmail.com>
2026-04-14 17:44:51 +0530

    Positive / Negative / Zero + Even/Odd + First vs Last program executed

--- HelloApp/src/main/java/org/example/Main.java
+++ HelloApp/src/main/java/org/example/Main.java
@@ -7,31 +7,41 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
 
-        // Fixed size array for 10 students
-        int[] ages = new int[10];
+        // Array of 5 numbers
+        int[] numbers = new int[5];
 
-        System.out.println("Enter ages of 10 students:");
+        System.out.println("Enter 5 numbers:");
 
         // Input with validation
-        for (int i = 0; i < ages.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = input.nextInt();
+        }
 
-            ages[i] = input.nextInt();
+        // Check each number
+        for (int i = 0; i < numbers.length; i++) {
 
-            // Validate age
-            if (ages[i] < 0) {
-                System.err.println("Invalid age entered!");
-                System.exit(0);
+            if (numbers[i] > 0) {
+
+                if (numbers[i] % 2 == 0) {
+                    System.out.println(numbers[i] + " is Positive and Even");
+                } else {
+                    System.out.println(numbers[i] + " is Positive and Odd");
+                }
+
+            } else if (numbers[i] < 0) {
+                System.out.println(numbers[i] + " is Negative");
+            } else {
+                System.out.println(numbers[i] + " is Zero");
             }
         }
 
-        // Check voting eligibility
-        for (int i = 0; i < ages.length; i++) {
-
-            if (ages[i] >= 18) {
-                System.out.println("Student " + (i + 1) + " can vote (Age: " + ages[i] + ")");
-            } else {
-                System.out.println("Student " + (i + 1) + " cannot vote (Age: " + ages[i] + ")");
-            }
+        // Compare first and last element
+        if (numbers[0] == numbers[4]) {
+            System.out.println("First and Last are Equal");
+        } else if (numbers[0] > numbers[4]) {
+            System.out.println("First is Greater than Last");
+        } else {
+            System.out.println("First is Less than Last");
         }
 
         input.close();
