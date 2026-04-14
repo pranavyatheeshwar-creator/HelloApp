11d6e834961076649ec294e2e8abdf08fc45fab9
SKPradeepp <skpiitmember2008@gmail.com>
2026-04-14 16:18:42 +0530

    Coded UC2: Display Hello with Command-Line Argument

--- HelloApp/src/main/java/org/example/Main.java
+++ HelloApp/src/main/java/org/example/Main.java
@@ -4,6 +4,12 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        if (args.length > 0) {
+            System.out.println("Hello, " + args[0] + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
+
     }
 }
