d5a71b3f5587e4dc6ccf2cc3b25bbb37a62a4efe
SKPradeepp <skpiitmember2008@gmail.com>
2026-04-14 16:43:33 +0530

    Added even/odd program

--- HelloApp/src/main/java/org/example/Main.java
+++ HelloApp/src/main/java/org/example/Main.java
@@ -1,15 +1,26 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+
+        if (input.hasNextInt()) {
+
+            int number = input.nextInt();
+
+            boolean isEven = (number % 2 == 0);
+
+            System.out.println("Is number even? " + isEven);
+
         } else {
-            String names = String.join(", ", args);
-            System.out.println("Hello, " + names + "!");
+            System.out.println("Invalid input. Please enter a number.");
         }
+
+        input.close();
     }
 }
