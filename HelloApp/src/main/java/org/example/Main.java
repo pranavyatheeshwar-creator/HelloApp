4a8a579e2eb74565094e6a21d30739de6eb4fbca
SKPradeepp <skpiitmember2008@gmail.com>
2026-04-14 16:32:27 +0530

    Coded UC7: Display Hello using String.join() method

--- HelloApp/src/main/java/org/example/Main.java
+++ HelloApp/src/main/java/org/example/Main.java
@@ -8,16 +8,7 @@ public class Main {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-
-            StringBuilder nameBuilder = new StringBuilder();
-
-            for (String name : args) {
-                nameBuilder.append(name).append(", ");
-            }
-
-            // Remove trailing ", "
-            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
-
+            String names = String.join(", ", args);
             System.out.println("Hello, " + names + "!");
         }
     }
