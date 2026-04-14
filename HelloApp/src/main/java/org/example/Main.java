5bbdce5e5c7d18be2355392fd34311e7610ec98d
SKPradeepp <skpiitmember2008@gmail.com>
2026-04-14 17:05:55 +0530

    Added Odd and Even Numbers from 1 to N program

--- HelloApp/src/main/java/org/example/Main.java
+++ HelloApp/src/main/java/org/example/Main.java
@@ -5,25 +5,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Scanner object for input
         Scanner input = new Scanner(System.in);
 
         // Take user input
         System.out.println("Enter a number:");
         int number = input.nextInt();
 
-        // Check condition using if-else if-else
-        if (number > 0) {
-            System.out.println("The number is Positive");
-        }
-        else if (number < 0) {
-            System.out.println("The number is Negative");
-        }
-        else {
-            System.out.println("The number is Zero");
+        // Check if valid natural number
+        if (number <= 0) {
+            System.out.println("Please enter a positive integer");
+        } else {
+
+            // Loop from 1 to number
+            for (int i = 1; i <= number; i++) {
+
+                // Check even or odd
+                if (i % 2 == 0) {
+                    System.out.println(i + " is Even");
+                } else {
+                    System.out.println(i + " is Odd");
+                }
+            }
         }
 
-        // Close scanner
         input.close();
     }
 }
